df0009e08172056ba8f75734d0ae190f9d6c2024
GuidoCeresa <gac@algos.it>
2014-11-19 12:09:40 +0100

    0.58 avanzamento di un ciclo

--- AlgoslibGrailsPlugin.groovy
+++ AlgoslibGrailsPlugin.groovy
@@ -3,7 +3,7 @@
 //
 class AlgoslibGrailsPlugin {
     // the plugin version
-    def version = "0.57"
+    def version = "0.58"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4.2 > *"
     // the other plugins this plugin depends on
